3f28c4025c6c55137056b24bc1066ceafa7535c3
ZabdiSong <166151048+ZabdiSong@users.noreply.github.com>
2024-08-29 13:28:11 +0800

    Update test777.py

--- test777.py
+++ test777.py
@@ -17,13 +17,13 @@ if answer == "基础设置":
     column1 = st.selectbox("设置选择", ("员工设置", "油品设置"))
 
 # Column 2
-if answer == "销售管理":
+elif answer == "销售管理":
     st.empty()
     st.title("销售管理")
     column2 = st.selectbox("设置选择" ("销售单制作（主管）", "分提单制作"))
 
 # Column 3
-if answer == "报表":
+elif answer == "报表":
     st.empty()
     st.title("报表")
     column3 = st.selectbox("设置选择", ("销售明细", "客存销售实时统计"))
