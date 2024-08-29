388305d69cc411f654b7358d98cd296abdc7ed15
ZabdiSong <166151048+ZabdiSong@users.noreply.github.com>
2024-08-29 11:31:26 +0800

    Update test777.py

--- test777.py
+++ test777.py
@@ -8,6 +8,7 @@ st.title("江苏国邦石油化工有限公司油库IC卡管理系统")
 
 # Set Columns
 answer = st.selectbox("分区选择", ("基础设置", "销售管理", "报表"))
+st.empty()
 
 # Column 1
 if answer == "基础设置":
@@ -15,16 +16,6 @@ if answer == "基础设置":
     st.title("基础设置")
     column1 = st.selectbox("设置选择", ("员工设置", "油品设置"))
 
-    # Set 1
-    if column1 == "员工设置":
-        st.empty()
-        st.title("员工设置")
-        set1 = st.selectbox("设置选择", ("新增", "修改", "删除"))
-     # Set 2
-    elif column1 == "油品设置":
-        st.empty()
-        st.title("油品设置")
-        set2 = st.selectbox("设置选择", ("新增", "修改", "启用", "禁用"))
-
+  
     
     
