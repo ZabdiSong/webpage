d055641b484c8971a3dff10a9cc21b6317961936
ZabdiSong <166151048+ZabdiSong@users.noreply.github.com>
2024-08-30 13:57:31 +0800

    Update test777.py

--- test777.py
+++ test777.py
@@ -14,5 +14,32 @@ if answer == "基础设置":
     st.empty()
     st.title("基础设置")
     column1 = st.selectbox("设置选择", ("员工设置", "油品设置"))
-
     
+    # Set 1
+    if column1 == "员工设置":
+        st.empty()
+        st.title("员工设置")
+        set1 = st.selectbox("设置选择", ("新增", "修改", "删除"))
+      
+        # Adding 
+        if set1 == "新增":
+            st.empty()
+            operate1 = int(input("输入项目"))
+            issue1 = st.selectbox("设置选择", ("确定", "取消"))
+         
+            if issue1 == "确定":
+                print(operate1)
+            
+            elif issue1 == "取消":
+                print(" ")
+
+
+    
+
+
+    
+
+
+
+
+
