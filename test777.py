a4765cf8cbec41eb71f4310f46b582b6ea932abb
ZabdiSong <166151048+ZabdiSong@users.noreply.github.com>
2024-08-30 13:46:59 +0800

    Update test777.py

--- test777.py
+++ test777.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-
 # Clear page
 st.empty()
 
@@ -12,31 +11,20 @@ answer = st.selectbox("分区选择", ("基础设置", "销售管理", "报表")
 
 # Column 1
 if answer == "基础设置":
-    st.title("基础设置")
-    column1 = st.selectbox("设置选择", ("员工设置", "油品设置"))
+    st.empty()
+    st.title("基础设置")
+    column1 = st.selectbox("员工设置", "油品设置")
 
-    # Set 1
-    if column1 == "员工设置":
-        st.title("员工设置")
-        set1 = st.selectbox("设置选择", ("新增", "修改", "删除"))
-
-    
-    # Adding 
-        if set1 == "新增":
-            operate1 = int(input("输入项目"))
-            issue1 = st.selectbox("设置选择", ("确定", "取消"))
-         
-            if issue1 == "确定":
-                print(operate1)
-            
-            elif issue1 == "取消":
-                print(" ")
-
-
-    
-    # Set 2   
-    elif column1 == "油品设置":
-        st.title("油品设置")
-        set2 = st.selectbox("设置选择", ("新增", "修改", "启用", "禁用"))
+    # Set 1
+    if column1 == "员工设置":
+        st.empty()
+        st.title("员工设置")
+        set1 = st.button("新增", "修改", "删除")
+    
+    # Set 2
+    elif column1 == "油品设置":
+        st.empty()
+        st.title("油品设置")
+        set2 = st.button("新增", "修改", "启用", "禁用")
 
 
